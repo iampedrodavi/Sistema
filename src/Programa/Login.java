e2131b694ed232f7a653d4ef050f68f7639b567c
Pedro Davi <pedrodavisousa@gmail.com>
2024-09-09 18:27:09 -0300

    UP TELA LOGIN

--- src/Programa/Login.java
+++ src/Programa/Login.java
@@ -37,6 +37,7 @@ public class Login extends JFrame {
 
 	
 	public Login() {
+		setTitle("LOGIN - RH");
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Login.class.getResource("/Programa/Imagens/recursos-humanos.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 305, 157);
