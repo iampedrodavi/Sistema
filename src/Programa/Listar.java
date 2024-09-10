9e2aaadceda9bf8e5c3f6048dbfaef5c492b057b
Pedro Davi <pedrodavisousa@gmail.com>
2024-09-09 22:44:55 -0300

    Modificaçao

--- src/Programa/Listar.java
+++ src/Programa/Listar.java
@@ -6,33 +6,40 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Toolkit;
 
+
 public class Listar extends JFrame {
 
     private static final long serialVersionUID = 1L;
     private JTextField txtSearch;
     private JTable tabela;
     private JScrollPane scrollPane;
-    
+
     public Listar() {
         setIconImage(Toolkit.getDefaultToolkit().getImage(Listar.class.getResource("/Programa/Imagens/recursos-humanos.png")));
-        setTitle("Listar Funcionários");
-        setSize(700, 500);
+        setTitle("Lista de Funcionários");
+        setSize(700, 485);
         setLocationRelativeTo(null);
         getContentPane().setLayout(null);
-        
-    
+
+       
         txtSearch = new JTextField();
-        txtSearch.setBounds(10, 400, 200, 30);
+        txtSearch.setBounds(136, 400, 200, 30);
         getContentPane().add(txtSearch);
-        
-     
+
+       
         JButton btnSearch = new JButton("Buscar");
-        btnSearch.setBounds(220, 400, 100, 30);
+        btnSearch.setBounds(346, 400, 100, 30);
         getContentPane().add(btnSearch);
-        
-   
+
+       
+        JButton btnAposentadoria = new JButton("Aposentadoria");
+        btnAposentadoria.setToolTipText("Para resetar a tabela, clicar novamente no botão BUSCAR!");
+        btnAposentadoria.setBounds(456, 400, 108, 30);
+        getContentPane().add(btnAposentadoria);
+
+       
         JButton btnBack = new JButton("Voltar");
-        btnBack.setBounds(330, 400, 150, 30);
+        btnBack.setBounds(574, 400, 100, 30);
         btnBack.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 new Menu().setVisible(true);
@@ -41,10 +48,8 @@ public class Listar extends JFrame {
         });
         getContentPane().add(btnBack);
 
-    
-        atualizarTabela("");
-
-    
+        atualizarTabela(""); 
+  
         btnSearch.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 String buscar = txtSearch.getText();
@@ -52,16 +57,23 @@ public class Listar extends JFrame {
             }
         });
 
+      
+        btnAposentadoria.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                mostrarFuncionariosPertoAposentadoria();
+            }
+        });
+
         setVisible(true);
     }
 
-    	private void atualizarTabela(String buscar) {
+    private void atualizarTabela(String buscar) {
         Conexao conexao = new Conexao("jdbc:mysql://localhost:3306/sistema", "root", "290600");
         List<Funcionario> funcionarios = buscar.isEmpty() ? conexao.buscarFuncionarios() : conexao.buscarFuncionariosPorNome(buscar);
-        
+
         String[] colunas = {"ID", "Nome", "Idade", "CPF", "E-mail", "Cargo"};
         String[][] dados = new String[funcionarios.size()][6];
-        
+
         for (int i = 0; i < funcionarios.size(); i++) {
             Funcionario func = funcionarios.get(i);
             dados[i][0] = String.valueOf(func.getIdFun());
@@ -71,16 +83,53 @@ public class Listar extends JFrame {
             dados[i][4] = func.getEmailFun();
             dados[i][5] = func.getCargoFun();
         }
-        
+
         if (scrollPane != null) {
-            getContentPane().remove(scrollPane); 
+            getContentPane().remove(scrollPane);
         }
-        
+
         tabela = new JTable(dados, colunas);
         scrollPane = new JScrollPane(tabela);
         scrollPane.setBounds(0, 0, 684, 395);
         getContentPane().add(scrollPane);
         
+        JLabel lblNewLabel = new JLabel("Nome \r\ndo Funcionário:");
+        lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
+        lblNewLabel.setBounds(10, 400, 136, 30);
+        getContentPane().add(lblNewLabel);
+
+        revalidate();
+        repaint();
+    }
+
+    
+    private void mostrarFuncionariosPertoAposentadoria() {
+        Conexao conexao = new Conexao("jdbc:mysql://localhost:3306/sistema", "root", "290600");
+        List<Funcionario> funcionariosPertoAposentadoria = conexao.Aposentadoria(); 
+
+        String[] colunas = {"ID", "Nome", "Idade", "CPF", "E-mail", "Cargo", "Anos para Aposentadoria"};
+        String[][] dados = new String[funcionariosPertoAposentadoria.size()][7];
+
+        for (int i = 0; i < funcionariosPertoAposentadoria.size(); i++) {
+            Funcionario func = funcionariosPertoAposentadoria.get(i);
+            dados[i][0] = String.valueOf(func.getIdFun());
+            dados[i][1] = func.getNomeFun();
+            dados[i][2] = String.valueOf(func.getIdadeFun());
+            dados[i][3] = func.getCpfFun();
+            dados[i][4] = func.getEmailFun();
+            dados[i][5] = func.getCargoFun();
+            dados[i][6] = String.valueOf(65 - func.getIdadeFun()); 
+        }
+
+        if (scrollPane != null) {
+            getContentPane().remove(scrollPane);
+        }
+
+        tabela = new JTable(dados, colunas);
+        scrollPane = new JScrollPane(tabela);
+        scrollPane.setBounds(0, 0, 684, 395);
+        getContentPane().add(scrollPane);
+
         revalidate();
         repaint();
     }
